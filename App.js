297d956b881c77ecc24abec7b396823431baa99f
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-03 23:59:03 +0530

    upto 59 minutes completed

--- App.js
+++ App.js
@@ -39,14 +39,23 @@ const Header = () => {
 }
 
 const RestaurantCard = ()=>{
-    
+    return(
+        <div className="res-cards">
+            <img className="res-logo" src="https://www.logodesign.net/logo-new/cartoon-food-truck-with-burger-on-top-9182ld.png?nwm=1&nws=1&industry=food&txt_keyword=All" />
+            <h3>Pizza Hut</h3>
+            <h2>Paneer Pizza </h2>
+            <h2>rating 4.4</h2>
+            <h2>38 minutes</h2>
+
+        </div>
+    )
 }
 
 const Body =()=>{
-    return <div className="body">
+    return <div className="body" style={{fontFamily:"Arial"}}>
         <div className="search-bar">Search</div>
         <div className="res-container">
-
+            <RestaurantCard/>
         </div>
     </div>
 }
