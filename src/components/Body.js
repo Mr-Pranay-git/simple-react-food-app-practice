d07231110c96f05449bf8eb9175b659666b8d455
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-09-08 23:25:43 +0530

    asyn me atak gya revise krna h

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import reslist from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
-    let [restaurantlist, SetRestaurantlist] = useState(reslist);
+    const [restaurantlist, SetRestaurantlist] = useState(reslist);
+
+    useEffect(()=>{
+       fetchData()
+    },[])
+
+    // const fetchData = async()=>{
+    //     const data =  fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1463&lng=79.0849&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        
+    // }
 
     return <div className="body" style={{ fontFamily: "Arial" }}>
         
         <button className="filter-btn" 
         onClick={()=>{
-          const filteredlist =  restaurantlist.filter((e)=>e.info.avgRating > 4.6)
+          const filteredlist =  restaurantlist.filter((res)=>res.info.avgRating > 4.6)
 
           SetRestaurantlist(filteredlist)
         }}>
