94ee289f9a080f3ac08c401f82c9be1c7a3a393b
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-11-17 18:39:51 +0530

    routing started

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
+import { createBrowserRouter } from "react-router-dom";
 
-    
 
 
 const AppLayout =()=>{
@@ -14,6 +14,20 @@ const AppLayout =()=>{
     </div>
 }
 
+const aapRouter = ([
+    {
+        path: "/",
+        element: <AppLayout/>
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+        path:"/contact",
+        element: <Contact/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<AppLayout/>)
