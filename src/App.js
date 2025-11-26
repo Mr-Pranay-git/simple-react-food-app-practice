e62c4131c494fdc1a51a7ddedc8a23f856d9d231
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-11-26 19:41:56 +0530

    started childe routs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ import Error from "./components/Error.js"
 const AppLayout =()=>{
     return <div className="app">
         <Header/>
+        {/* if path = / */}
         <Body/> 
+        {/* if path = /about */}
+        <About/>
+        {/* if path = /contact */}
+        <Contact/>
     </div>
 }
 
