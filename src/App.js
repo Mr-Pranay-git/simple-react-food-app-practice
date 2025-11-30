127a64d228ecf750d87405cdc31c74bb429a7f8c
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-12-01 02:30:50 +0530

    uses outlet for routing

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact.js";
 import Error from "./components/Error.js"
 
 
-const AppLayout =()=>{
-    return <div className="app">
-        <Header/>
-        {/* if path = / */}
-        <Body/> 
-        {/* if path = /about */}
-        <About/>
-        {/* if path = /contact */}
-        <Contact/>
+const AppLayout = () => {
+    return (<div className="app">
+        
+        <Header />
+        <Outlet/> 
     </div>
+    )
 }
 
 const aapRouter = createBrowserRouter([
     {
         path: "/",
-        element: <AppLayout/>,
-        errorElement:<Error/>
+        element: <AppLayout />,
+        children: [     
+            {
+                path:'/',
+                element:<Body />
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ],
+        errorElement: <Error />
     },
-    {
-        path: "/about",
-        element: <About/>,
-    },
-    {
-        path: "/contact",
-        element: <Contact/>
-    }
+
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
