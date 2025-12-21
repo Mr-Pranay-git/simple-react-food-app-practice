ed7e73501b53f5f4936e570280c784172a8ca162
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-12-21 23:58:10 +0530

    functional components

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import UserClass from './UserClass'
+import User from './User'
 
 const About = () => {
   return (
     <div>
       <h1>about us </h1>
+      <User />
+      <UserClass  />
     </div>
   )
 }
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const User = (name) => {
+  
+  return (
+    <div className='user-card'>
+            <h2>Name:{name} </h2>
+            <h3>Location: Deharadun</h3>
+            <h4>Contact: @parmanu777</h4>
+        </div>
+  )
+}
+
+export default User;
