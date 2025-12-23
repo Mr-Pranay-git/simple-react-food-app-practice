3e5e153d82fc6479c606c96ffbf35a86ea0279ca
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-12-24 00:00:26 +0530

    border added in both about

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -10,7 +10,7 @@ class UserClass extends React.Component {
     render() {
         return (
             <div className="user-card">
-                <h2>Name: Parmanu</h2>
+                <h2>Name:{this.props.name} </h2>
                 <h3>Location: Deharadun</h3>
                 <h4>Contact: @parmanu777</h4>
             </div>
